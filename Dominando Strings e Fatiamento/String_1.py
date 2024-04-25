cea43fc9e457793d435d3f6dd16a09fe71ace94b
RodriguesMarllon <marllonrsantos@gmail.com>
2024-04-25 17:46:34 -0300

    Conhecendo métodos úteis da classe String

---
+++ Dominando Strings e Fatiamento/String_1.py
@@ -0,0 +1,21 @@
+# nome = "Marllon"
+
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.title())
+
+# print()
+
+# texto = " Olá mundo!        "
+
+# print(texto + ".")
+# print(texto.strip() + ".")
+# print(texto.lstrip() + ".")
+# print(texto.rstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
