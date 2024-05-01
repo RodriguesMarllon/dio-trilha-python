33f7fd43db14cbe2718e7ce6a2f33c896a75c4db
RodriguesMarllon <marllonrsantos@gmail.com>
2024-05-01 12:27:49 -0300

    Polimorfismo com Herança

---
+++ Orientação a Objetos e Boas práticas em Python/06 - Polimorfismo/01_polimorfismo.py
@@ -0,0 +1,27 @@
+class Passaro:
+    def voar(self):
+        print("Voando...")
+
+
+class Pardal(Passaro):
+    def voar(self):
+        return super().voar()
+    
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Avestruz não pode voar")
+
+
+# FIXME: Exemplo ruim do uso de herança para "ganhar" o método voar
+class Aviao(Passaro):
+    def voar(self):
+        print("Avião está decolando...")
+
+def plano_voo(objeto):
+    objeto.voar()
+
+
+plano_voo(Pardal())
+plano_voo(Avestruz())
+plano_voo(Aviao())
