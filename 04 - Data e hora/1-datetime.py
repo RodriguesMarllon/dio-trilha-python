f46919cf93aee3ec61e94628ea52a77c712340a5
RodriguesMarllon <marllonrsantos@gmail.com>
2024-05-09 20:36:49 -0300

    Trabalhando com objetos date, datetime e time

---
+++ 04 - Data e hora/1-datetime.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime, time
+
+data = date(2024, 8, 7)
+print(data)
+
+print(date.today())
+
+data_hora = datetime(2024, 8, 7)
+print(data_hora)
+
+print(datetime.now())
+
+hora = time(10, 20, 30)
+print(hora)
