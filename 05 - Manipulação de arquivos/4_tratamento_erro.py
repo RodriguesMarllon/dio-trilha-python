3141cf049aac19ba4329c9628cb2284b469407a5
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-05-30 22:17:46 -0300

    Tratamento de exeções em manipulação de arquivos

---
+++ 05 - Manipulação de arquivos/4_tratamento_erro.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+try:
+    arquivo = open("meu_arquivo.py")
+except FileNotFoundError as exc:
+    print("Arquivo não encontrado") 
+    print(exc)
+
+ROOT_PATH = Path(__file__).parent
+
+try:
+    arquivo = open(ROOT_PATH / "novo-diretorio")
+except Exception as exc:
+    print(f"Não foi possível abrir o arquivo: {exc}")
+    
